6b70f87e70ca79ebfca0b7fd57b8ce9e4fbb2c96
Shivani Bhardwaj <shivanib134@gmail.com>
2016-08-01 23:58:37 +0530

    Reverse a linked list using iteration

---
+++ karum/ReverseLL(II).cpp
@@ -0,0 +1,81 @@
+#include<iostream>
+
+using namespace std;
+
+class Node{
+        public:
+                int data;
+                Node *next;
+
+                Node(){}
+
+                Node(int d){
+                        data=d;
+                        next=NULL;
+                }
+
+                Node *insertElement(Node *head,int d){
+                        Node *np=new Node(d);
+                        Node *tmp=head;
+
+                        if(head==NULL)
+                                return np;
+                        else
+                                while(tmp->next)
+                                        tmp=tmp->next;
+
+                        tmp->next=np;
+
+                        return head;
+                }
+
+		Node *reverseList(Node *head){
+			Node *tmp;
+
+			while(head){
+				Node *tq=head->next;
+				head->next=tmp;
+				tmp=head;
+				head=tq;
+			}
+
+			return tmp;
+		}
+
+		void displayList(Node *head){
+                        Node *tmp=head;
+
+                        if(head==NULL)
+                                cout<<"No elements in the list! :(\n";
+                        else{
+                                cout<<"Elements are: \n";
+                                while(tmp){
+                                        cout<<tmp->data<<"\n";
+                                        tmp=tmp->next;
+                                }
+                        }
+                }
+};
+
+int main()
+{
+        int n,p,pos,d;
+        Node np;
+        Node *head=NULL;
+
+        cout<<"Enter the size of linked list: ";
+        cin>>n;
+
+        for(int i=0;i<n;i++){
+                        cout<<"\nEnter element "<<i+1<<": ";
+                        cin>>p;
+                        head=np.insertElement(head,p);
+                }
+        np.displayList(head);
+
+        head=np.reverseList(head);
+
+        cout<<"After reversing, ";
+
+        np.displayList(head);
+}
